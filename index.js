3c59ca8389e624700e2b5ec24b00a7728d0ecb96
AbdulRehmankm <168673922+AbdulRehmankm@users.noreply.github.com>
2025-05-09 06:36:46 -0700

    Update index.js
    add vegamovies link

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ setInterval(reloadWebsite, interval);
 const allowedOrigins = [
   'https://snapmoviehd.com',
   'https://www.snapmoviehd.com',
+  'https://vegamovies.snapmoviehd.com',
+  'https://www.vegamovies.snapmoviehd.com',
   'http://localhost:3000'
 ];
 
